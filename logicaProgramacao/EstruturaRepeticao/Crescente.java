b857e8f142edd2e05a217478d2557e75f60c197f
J-Breno <joao.breno85@hotmail.com>
2024-02-20 13:39:34 -0300

    Exercicio Crescente

---
+++ logicaProgramacao/EstruturaRepeticao/Crescente.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+
+public class Crescente {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y;
+		System.out.println("Digite dois numeros: ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		if(x > y) {
+			System.out.println("Decrescente!");
+		} else {
+			System.out.println("Crescente!");
+		}
+		
+		while(x != y) {
+			System.out.println("Digite dois numeros: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+			
+			if(x > y) {
+				System.out.println("Decrescente!");
+			} else if(y < x) {
+				System.out.println("Crescente!");
+			}
+		}
+	}
+}
